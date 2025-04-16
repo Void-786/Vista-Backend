edfa413c6af82be900e570faeb1ea2c549708443
Faiz Ullah Khan <faizullahkhan2209@gmail.com>
2025-04-17 01:02:56 +0530

    fixed

--- src/main/java/com/project/car_rental_services/security/SecurityConfig.java
+++ src/main/java/com/project/car_rental_services/security/SecurityConfig.java
@@ -26,7 +26,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .cors()
+                .and()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/admin/**").authenticated()
                         .anyRequest().permitAll()
